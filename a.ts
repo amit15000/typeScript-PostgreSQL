c100ad978c338f6ce4104fc781ce7ed42a2ece77
Amit Kumar <15000amitkumar@gmail.com>
2024-04-18 21:46:31 +0530

    Passing function

--- a.ts
+++ a.ts
@@ -1,3 +1,14 @@
-const a : number = 10;
-// a="Amit";
-console.log(a);
+// const a : number = 10;
+// // a="Amit";
+// console.log(a);
+
+
+
+
+function runScript(fn:()=>void ){
+    setTimeout(fn, 2000);
+}
+
+runScript(function (){
+    console.log("Amit")
+})
