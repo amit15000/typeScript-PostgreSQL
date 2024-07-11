ed604b8d0e33590c7c6d35b24daf301e6b0161f7
Amit Kumar <15000amitkumar@gmail.com>
2024-07-11 17:26:44 +0530

    funciton passed in function

--- src/basic.ts
+++ src/basic.ts
@@ -8,23 +8,22 @@
 // function add2 ( a:number, b:number):number{
 //     return a+b;
 // }
-function runScript(fn: () => void) {
-  setTimeout(fn, 2000);
-}
-
-runScript(function () {
-  console.log("Amit");
-});
-
-// // "noImplicitAny": false,
-// function myF (a,b){
-//     return a;
-
+// function runScript(fn: () => void) {
+//   setTimeout(fn, 2000);
 // }
 
-// function add3 ( fn:(a:number,b:string)=>number):number{
-//     setTimeout(fn,1000)
-//     return 3;
+// runScript(function () {
+//   console.log("Amit");
+// });
+
+// "noImplicitAny": false,
+// function myF(a, b) {
+//   return a;
+// }
+
+// function add3(fn: (a: number, b: string) => number): number {
+//   setTimeout(fn, 1000);
+//   return 3;
 // }
 
 // add3(myF);
@@ -61,10 +60,26 @@ runScript(function () {
 //     age:19
 // })
 
-// const product = (a:number,b:number):Number =>{
-//     return a*b;
-// }
-
-// const product = (fn: (a: number, b: number) => number): number => {
-//   return 25;
+// const product = (a: number, b: number): Number => {
+//   return a * b;
 // };
+// console.log(product(12, 23));
+
+const fproduct = (
+  fn: (a: number, b: number) => number,
+  callback: (result: number) => void
+): void => {
+  setTimeout(() => {
+    const r = fn(3, 4); // Example arguments for the function
+    console.log(r);
+    const result = r - 25;
+    callback(result);
+  }, 2000);
+};
+
+// Example usage
+const multiply = (a: number, b: number): number => a * b;
+
+const result = fproduct(multiply, (result) => {
+  console.log(result);
+});
