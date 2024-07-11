7df232f7a67dfd7a108ca02ad7b27f05daf7858e
Amit Kumar <15000amitkumar@gmail.com>
2024-07-11 19:28:58 +0530

    Map

--- src/advanceAPIs.ts
+++ src/advanceAPIs.ts
@@ -1,8 +1,8 @@
-interface User {
-  readonly name: string;
-  age: number;
-  email?: string;
-}
+// interface User {
+//   readonly name: string;
+//   age: number;
+//   email?: string;
+// }
 
 // type updatedUser = Pick<User, "email" | "age">;
 
@@ -16,19 +16,61 @@ interface User {
 //   console.log("asdsad");
 // }
 
-type UserS = {
+// type UserS = {
+//   name: string;
+//   age: number;
+// };
+
+// const user1: Readonly<UserS> = {
+//   name: "Amit",
+//   age: 132,
+// };
+
+// // user1.name = "sadasd";
+
+// const user2: Readonly<User> = {
+//   name: "Sumit",
+//   age: 12,
+// };
+
+//                                                        Record
+
+// Complex Way
+
+// type Users = {
+//   [key: string]: {
+//     readonly name: string;
+//     age: number;
+//   };
+// };
+
+// Easier Way by using Record
+
+// type Users = Record<string, { name: string; age: number }>;
+
+// const users: Users = {
+//   id1: {
+//     name: "Amit",
+//     age: 12,
+//   },
+//   id2: {
+//     name: "Amit",
+//     age: 12,
+//   },
+// };
+
+//                                                                 Map
+
+type User = {
   name: string;
   age: number;
+  email: string;
 };
+const users = new Map<string, User>();
 
-const user1: Readonly<UserS> = {
-  name: "Amit",
-  age: 132,
-};
-
-// user1.name = "sadasd";
-
-const user2: Readonly<User> = {
-  name: "Sumit",
-  age: 12,
-};
+users.set("user1", { name: "Amit", age: 12, email: "amit@sumit.com" });
+users.set("user2", { name: "Amit", age: 12, email: "amit@sumit.com" });
+// users.set("user3", { name: "Amit", age: 12 });     // Error bcz email is not partial
+console.log(users.get("user1"));
+users.delete("user1");
+console.log(users.get("user1"));
