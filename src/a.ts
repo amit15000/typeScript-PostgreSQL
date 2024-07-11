6800fc2cfc0912b5f1b51015ffff364aedc73de2
Amit Kumar <15000amitkumar@gmail.com>
2024-07-11 20:01:37 +0530

    Type Inferences In Zod

--- src/a.ts
+++ src/a.ts
@@ -2,3 +2,14 @@ const a = 1;
 export const b = 12;
 
 export default a;
+
+import z, { number, string } from "zod";
+
+const StringZodSchema = z.string().min(2, "Atleast 2 size");
+
+type StringZodType = z.infer<typeof StringZodSchema>;
+
+type X = {
+  age: number;
+  name: string;
+};
